edd974b4dc8046914ecdb43e48f4640d85fa9bf9
98765kjh <98765kjh@gmail.com>
2017-02-01 17:57:57 +0900

    CNN MNIST, save and restore

--- 11_CNN.py
+++ 11_CNN.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import input_data
+import os
 
 def init_weights(shape):
     w = tf.Variable(tf.random_normal(shape, stddev=0.01))
     return w
-"""
+
 def model(X, w1, w2, w3, w4, w_out, p_keep_conv, p_keep_hidden):
     l1a = tf.nn.relu(tf.nn.conv2d(X, w1, strides=[1,1,1,1], padding='SAME'))
     l1 = tf.nn.max_pool(l1a, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
@@ -24,7 +25,7 @@ def model(X, w1, w2, w3, w4, w_out, p_keep_conv, p_keep_hidden):
     l4 = tf.nn.dropout(l4, p_keep_hidden)
     pyx = tf.matmul(l4, w_out)
     return pyx
-"""
+
 batch_size = 128
 test_size = 256
 
@@ -53,7 +54,7 @@ w_out = init_weights([625, 10])
 
 p_keep_conv = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
-
+"""
 l1a = tf.nn.relu(tf.nn.conv2d(X, w1, strides=[1,1,1,1], padding='SAME'))
 l1 = tf.nn.max_pool(l1a, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 l1 = tf.nn.dropout(l1, p_keep_conv)
@@ -70,27 +71,42 @@ l3 = tf.nn.dropout(l3, p_keep_conv)
 l4 = tf.nn.relu(tf.matmul(l3, w4))
 l4 = tf.nn.dropout(l4, p_keep_hidden)
 py_x = tf.matmul(l4, w_out)
-#py_x = model(X, w1, w2, w3, w4, w_out, p_keep_conv, p_keep_hidden)
+"""
+
+py_x = model(X, w1, w2, w3, w4, w_out, p_keep_conv, p_keep_hidden)
 
 #print py_x
 
 # using Softmax & optimization
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
 optimizer = tf.train.RMSPropOptimizer(0.01, 0.9).minimize(cost)
-init = tf.global_variables_initializer()
 predict_op = tf.argmax(py_x, 1)
 
 #accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(py_x, 1), tf.argmax(Y, 1)), tf.float32))
 
+ckpt_dir = "./save"
+if not os.path.exists(ckpt_dir):
+    os.makedirs(ckpt_dir)
+
+global_step = tf.Variable(0, name='global_step', trainable=False)
+
+# after declare all tf.Variables, call saver
+saver = tf.train.Saver()
 
 with tf.Session() as sess:
 
-    sess.run(init)
+    tf.initialize_all_variables().run()
 
-    for i in range(100):
-        training_batch = zip(range(0, len(trX), batch_size),\
-                            range(batch_size, len(trX)+1, batch_size))
-        print training_batch[-1]
+    ckpt = tf.train.get_checkpoint_state(ckpt_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        print(ckpt.model_checkpoint_path)
+        saver.restore(sess, ckpt.model_checkpoint_path)
+
+    start = global_step.eval()
+    print("Start from:", start)
+
+    for i in range(start, 100):
+        training_batch = zip(range(0, len(trX), batch_size), range(batch_size, len(trX)+1, batch_size))
         for start, end in training_batch:
             sess.run(optimizer, feed_dict={X:trX[start:end], Y:trY[start:end], p_keep_conv:0.8, p_keep_hidden:0.5})
 
@@ -99,7 +115,13 @@ with tf.Session() as sess:
                 print ', cost:', sess.run(cost, feed_dict={X:trX[start:end], Y:trY[start:end], p_keep_conv:0.8, p_keep_hidden:0.5}),
                 print ']'
 
+        global_step.assign(i).eval()    # set and eval(update) global_step with index(i)
+        saver.save(sess, ckpt_dir+"/model.ckpt", global_step=global_step)
+        print(i, np.mean(np.argmax(teY, axis=1) ==\
+                            sess.run(predict_op, feed_dict={X: teX,\
+                                                            p_keep_conv:1.0, p_keep_hidden:1.0})))
 
+"""
         test_indices = np.arange(len(teX))
         np.random.shuffle(test_indices)
         test_indices = test_indices[0:test_size]
@@ -108,6 +130,8 @@ with tf.Session() as sess:
         print i, np.mean(np.argmax(teY[test_indices], axis=1) ==\
                         sess.run(predict_op, feed_dict={X: teX[test_indices],\
                         p_keep_conv:1.0, p_keep_hidden:1.0}))
+"""
+
 """
 with tf.Session() as sess:
     sess.run(init)
